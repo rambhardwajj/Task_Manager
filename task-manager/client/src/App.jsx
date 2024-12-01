8d3b1fe8c626f1328a35fbfd085d5887d4607a2e
Bhardwaj <rabhardw@ciena.com>
2024-12-02 03:19:57 +0530

    added static Layout component

--- task-manager/client/src/App.jsx
+++ task-manager/client/src/App.jsx
@@ -8,6 +8,32 @@ import Trash from "./pages/Trash.jsx"
 import TaskDetails from "./pages/TaskDetails.jsx"
 import { Toaster } from 'sonner'
 
+function Layout(){
+  const { user } = "";
+  const location = useLocation();
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+        {/* <Sidebar /> */}
+      </div>
+
+      {/* <MobileSidebar /> */}
+
+      <div className='flex-1 overflow-y-auto'>
+        <Navbar />
+
+        <div className='p-4 2xl:px-10'>
+          {/* <Outlet /> */}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace />
+  );
+
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
